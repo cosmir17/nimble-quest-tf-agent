9401062065b90084e585c755cbeb810f3e44bcf4
cosmir <cosmir@cosmir>
2020-12-22 16:33:50 +0000

    added more screenshots

--- screen_classifier/stage_classifier/game_stage.py
+++ screen_classifier/stage_classifier/game_stage.py
@@ -4,7 +4,7 @@ from enum import Enum
 class GameStage(Enum):
     in_progress = 0
     game_over = 1
-    starting_page = 2
+    starting_page = 2 # character selection page
     interval = 3
 
     character_upgrade = 4
@@ -13,6 +13,6 @@ class GameStage(Enum):
     paused_game_while_in_progress = 7
     game_over_sorry = 8
     store_page = 9
-    main_page = 10
+    main_page = 10 # first page
 
     died = 11
